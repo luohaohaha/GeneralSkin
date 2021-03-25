347007ef1351d9bbafb8289e31cf0bb6b3be00ad
罗昊 <luohaohaha@gmail.com>
2021-03-25 11:04:49 +0800

    Update ColorModule.java

--- app/src/main/java/saltyfish/generalskin/ColorModule.java
+++ app/src/main/java/saltyfish/generalskin/ColorModule.java
@@ -1,17 +1,6 @@
 package saltyfish.generalskin;
 
-/**
- * Project: CCBClient_V4.00<br/>
- * Package: com.ccb.setskin<br/>
- * ClassName: ColorModule<br/>
- * Description: 颜色模型数据 <br/>
- * Date: 2016-9-5 9:23 <br/>
- * <p>
- * Author 昊<br/>
- * Version 1.0<br/>
- * since JDK 1.6<br/>
- * <p>
- */
+
 
 public class ColorModule {
 
